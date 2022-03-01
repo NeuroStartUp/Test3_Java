60b0ea601726f3bb2bc2a91e60150fb4cba226b7
Duinova <Ganenok80@mail.ru>
2022-03-01 18:49:32 +0700

    доработка ДЗ

--- src/main.java
+++ src/main.java
@@ -1,18 +1,16 @@
-import java.util.Arrays;
-
-public class main {
+public class Main {
     public static void main(String[] args) {
 
         int score = 200; // Счет абонента
         System.out.println("Сумма на счете до зачисления: " + (score) + " рублей");
 
-        int Replenishment = 1500; // Сумма пополнения счета
+        int Replenishment = 1000; // Сумма пополнения счета
         System.out.println("Внесено на счет: " + (Replenishment) + " рублей");
 
         int bonus = 100; // шаг бонуса начисления
 
         if (Replenishment >= 1000) {
-            System.out.println("Начислено бонусов:" + (Replenishment / bonus) + " рублей");
+            System.out.println("Начислено бонусов: " + (Replenishment / bonus) + " рублей");
             System.out.println("Итого на счете: " + (Replenishment + score + (Replenishment / bonus)) + " рублей");
         } else {
             System.out.println("Итого на счете: " + (Replenishment + score) + " рублей");
